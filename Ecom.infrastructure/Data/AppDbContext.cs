4e48e98c794b2f8fc64045e3ff6f3586f976681c
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-03-05 02:14:13 +0300

    Add configuration and applies Configuration

--- Ecom.infrastructure/Data/AppDbContext.cs
+++ Ecom.infrastructure/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +22,8 @@ namespace Ecom.infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //تعريف ال Cofiguration
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
---
+++ Ecom.infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Ecom.Core.Entites.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Description).IsRequired();
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
