f3577d1bee99acd6a64b83293c16db3034a31972
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-04-21 00:55:11 +0300

    Complete Email Send Service

--- Ecom.infrastructure/Repositries/Service/EmailService.cs
+++ Ecom.infrastructure/Repositries/Service/EmailService.cs
@@ -1,5 +1,7 @@
 ﻿using Ecom.Core.Dto;
 using Ecom.Core.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,42 @@ namespace Ecom.infrastructure.Repositries.Service
 {
     public class EmailService : IEmailService
     {
-        public Task SendEmail(EmailDto emailDto)
+        private readonly IConfiguration configuration;
+        public EmailService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            this.configuration = configuration;
+        }
+        public async Task SendEmail(EmailDto emailDto)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress("My Ecom", configuration["EmailSetting:From"]));
+            mimeMessage.Subject = emailDto.Subject;
+            mimeMessage.To.Add(new MailboxAddress(emailDto.To,emailDto.To));
+            mimeMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = emailDto.Content
+            };
+            using(var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    await smtp.ConnectAsync(
+                        configuration["EmailSetting:Smtp"],
+                        int.Parse(configuration["EmailSetting:Port"]), true);
+                    await smtp.AuthenticateAsync(configuration["EmailSetting:Username"],
+                        configuration["EmailSetting:Password"]);
+                    await smtp.SendAsync(mimeMessage);
+                }
+                catch (Exception ex) 
+                {
+                    throw;
+                }
+                finally
+                {
+                    smtp.Disconnect(true);
+                    smtp.Dispose();
+                }
+            }
         }
     }
 }
