78a51ceccbac2937120d42cb70282ee8d3f9c437
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-04-23 06:41:58 +0300

    Create Active Account Endpoint with forget password

--- Ecom.Api/Controllers/AccountController.cs
+++ Ecom.Api/Controllers/AccountController.cs
@@ -27,6 +27,10 @@ namespace Ecom.Api.Controllers
         public async Task<IActionResult> login(LoginDto loginDto)
         {
             var result = await work.Auth.LoginAsync(loginDto);
+            if (result.StartsWith("please"))
+            {
+                return BadRequest(new ResponseAPI(400,result));
+            }
             Response.Cookies.Append("token", result, new CookieOptions
             {
                 Secure = true,
@@ -37,6 +41,18 @@ namespace Ecom.Api.Controllers
                 SameSite = SameSiteMode.Strict,
             });
             return Ok(new ResponseAPI(200,result));
+        }
+        [HttpPost("active-account")]
+        public async Task<IActionResult> active(ActiveAccountDto accountDto)
+        {
+            var result = await work.Auth.ActiveAccount(accountDto);
+            return result ? Ok(new ResponseAPI(200)) : BadRequest(new ResponseAPI(200));
+        }
+        [HttpPost("send-email-forget-password")]
+        public async Task<IActionResult> forget(string email)
+        {
+            var result = await work.Auth.SendEmailForForgetPassword(email);
+            return result ? Ok(new ResponseAPI(200)) : BadRequest(new ResponseAPI(200));
         }
     }
 }
